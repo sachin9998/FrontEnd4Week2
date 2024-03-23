328d1fa32e510742ab75f6428c7e25f6d1a0a244
Sachin Alam <sachinalam4216@gmail.com>
2024-03-23 21:40:16 +0530

    Final Version

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "../Styles/profile.css";
 import ProfileSVG from "../Assets/user-1-svgrepo-com.svg";
 
-const Profile = ({userDetails,setUserDetails}) => {
+const Profile = ({ userDetails, setUserDetails }) => {
+  useEffect(() => {
+    // Fetch user details from local storage when the component mounts
+    const storedUser = JSON.parse(localStorage.getItem("user Data"));
+    if (storedUser) {
+      setUserDetails(storedUser);
+    }
+  }, []);
 
-    useEffect(() => {
-        // Fetch user details from local storage when the component mounts
-        const storedUser = JSON.parse(localStorage.getItem('user Data'));
-        if (storedUser) {
-          setUserDetails(storedUser);
-        }
-      }, []);
-
-    const handleLogout = () => {
-        // Clear user data from local storage on logout
-        localStorage.removeItem('user Data');
-        window.location.href = '/';
-      };
+  const handleLogout = () => {
+    // Clear user data from local storage on logout
+    localStorage.removeItem("user Data");
+    window.location.href = "/";
+  };
 
   return (
     <div className="main">
@@ -32,7 +31,9 @@ const Profile = ({userDetails,setUserDetails}) => {
           <p>Full Name: {userDetails.firstName + " " + userDetails.lastName}</p>
           <p>Email: {userDetails.email}</p>
         </div>
-        <button className="logout-button" onClick={handleLogout}>LOGOUT</button>
+        <button className="logout-button" onClick={handleLogout}>
+          LOGOUT
+        </button>
       </div>
     </div>
   );
